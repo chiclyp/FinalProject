483663a5be112a52489e9f98ea08df8ed53eab7d
Pauline <chicoyepauline@gmail.com>
2020-05-09 21:20:17 -0400

    Added setters/getters + Part of the welcome page

--- AtmProject/src/main/java/Atm/ATM.java
+++ AtmProject/src/main/java/Atm/ATM.java
@@ -1,5 +1,7 @@
 package Atm;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Pauline Chicoye
@@ -7,7 +9,17 @@ package Atm;
 public class ATM {
     
     public static void main(String[] args) {
+       Account details = new Account(1000.00);
+       
+        System.out.println("Hello. Welcome to your virtual ATM machine.");
+        System.out.println("Let's start by having you provide your full name");
         
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        
+        System.out.println("Welcome back " + name + " What is your nip?");
+        
+        int nip = scan.nextInt();
     }
     
 }
--- AtmProject/src/main/java/Atm/Account.java
+++ AtmProject/src/main/java/Atm/Account.java
@@ -5,5 +5,35 @@ package Atm;
  * @author Pauline Chicoye
  */
 public class Account {
+    private final int ACC_NIP = 11458;
+    private double accBalance;
+    private double depositSum;
+    private double withdrawSum;
+    
+    public Account(double aB){
+        aB = accBalance;
+    }
+    public void setBalance (double bal){
+        this.accBalance = bal;
+    }
+    public void setDepSum (double dep){
+        this.depositSum = dep;
+    }
+    public void setWithSum (double with){
+        this.withdrawSum = with;
+    }
+    public double getBalance(){
+        return accBalance;
+    }
+    public double getDepSum() {
+        return depositSum;
+    }
+    public double getWithSum(){
+        return withdrawSum;
+    }
+    @Override
+    public String toString(){
+        return "Your account balance is " + accBalance;
+    }
     
 }
